e175be40ae51249ab144c68f395d0e6b6f75c640
dingzhencong <dingzhencong@gmail.com>
2018-03-27 20:42:55 +0800

    新增注释

--- jdk/src/java.base/share/classes/java/lang/Object.java
+++ jdk/src/java.base/share/classes/java/lang/Object.java
@@ -84,6 +84,35 @@ import jdk.internal.HotSpotIntrinsicCandidate;
  *
  * 总结 Object native 方法总共有7个，分别是clone,hashcode,getClass,notify,notifyAll,wait,registerNatives.
  *
+ *  对这些方法功能进行简单的描述：
+ *
+ *   （一）Object()：默认构造方法
+ *
+ *   （二）clone()：创建并返回此对象的一个副本
+ *
+ *   （三）equals()：指示某个其他对象是否与此对象相等
+ *
+ *   （四）finalize()：当垃圾回收器确定不存在对该对象的更多引用时，由对象的垃圾回收器调用此方法
+ *
+ *   （五）getClass()：返回一个对象的运行时类
+ *
+ *   （六）hashCode()：返回该对象的哈希值
+ *
+ *   （七）notify()：唤醒此对象监视器上等待的单个线程
+ *
+ *   （八）notifyAll()：唤醒此对象监视器上等待的所有线程
+ *
+ *   （九）toString()：返回该对象的字符串表示
+ *
+ *   （十）wait()：导致当前的线程等待，直到其它线程调用此对象的notify()或notifyAll()
+ *
+ *   （十一）wait(long timeout)：导致当前的线程等待调用此对象的notify()或notifyAll()
+ *
+ *   （十二）wait(long timeout, int nanos)：导致当前的线程等待，直到其他线程调用此对象的notify()或notifyAll()，或其他某个线程中断当前线程，或者已超过某个实际时间量
+ *
+ *   （十三）
+ *  registerNatives()：对本地方法进行注册
+ *  
  */
 public class Object {
 
